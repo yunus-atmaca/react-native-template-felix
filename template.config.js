97821029cba65a8f1db163ae923cb30c38aa5a13
yunus-atmaca <yunusatmacabm@gmail.com>
2021-04-21 14:53:26 +0300

    initial

---
+++ template.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  placeholderName: 'Felix',
+  templateDir: './template',
+}
---
+++ template/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  View
+} from 'react-native'
+
+function App() {
+  return (
+    <View style={{ flex: 1 }}>
+
+    </View>
+  )
+}
+
+export default App
